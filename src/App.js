b890ea8e4dd3edbb2b29bd11306797cd8fc72c78
kaleem <mokaleem@users.noreply.github.com>
2023-06-18 00:15:04 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Forecast from "./components/Forecast";
+
+function App() {
+  return (
+    <div className="App">
+      <Forecast />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Forecast.js
@@ -0,0 +1,8 @@
+import React from "react";
+import DataTable from "./DataTable";
+
+const Forecast = () => {
+  return <DataTable />;
+};
+
+export default Forecast;
